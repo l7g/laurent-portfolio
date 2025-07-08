94b655244a678e425a98736c923589806279affb
Laurent Gagné <51474008+l7g@users.noreply.github.com>
2025-07-08 02:23:59 +0200

    fix: improve health check API by handling potential errors in database counts and updating connection test response

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,12 @@ declare global {
 export const prisma =
   globalThis.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: process.env.NODE_ENV === "development" ? ["query"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
